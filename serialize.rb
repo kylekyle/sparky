3d1b912c90eddec55b9f8c5cc69c7f8e80350ea0
Kyle King <kylejking@gmail.com>
2015-07-01 18:07:18 -0600

    serialize anonymous classes

--- serialize.rb
+++ serialize.rb
@@ -1,28 +1,38 @@
+require 'tmpdir'
 require 'jruby/core_ext'
 
 import 'org.apache.spark.SparkConf'
 import 'org.apache.commons.io.IOUtils'
+import 'org.apache.spark.api.SparkContext'
 import 'org.apache.spark.api.java.JavaSparkContext'
 import 'org.apache.spark.api.java.function.FlatMapFunction'
 
 configuration = SparkConf.new.setAppName "JavaWordCount"
-context = JavaSparkContext.new configuration
-
-lines = context.textFile 'words.txt', 1
-
-class SplitLines
-  include FlatMapFunction
-
-  def call line
-    line.split
-  end
-
-  become_java!
-end
+context = SparkContext.new configuration
 
 split = Java::SparkyFlatMapFunction.new 
-split.klass = File.read('rubyobj/SplitLines.class').to_java_bytes
-split.instance = Java::Sparky.serialize SplitLines.new
 
-split = lines.flatMap split
-split.collect.each {|result| p result}
+dir = Dir.mktmpdir
+
+block = proc do |line|
+	line.split
+end
+
+klass = Class.new do
+  define_method :call, &block
+  become_java! dir
+end
+
+java_class_name = klass.java_class.get_name
+ruby_class_name = java_class_name[/[A-Z].+/]
+java_class_path = File.join dir, *java_class_name.split('.')
+
+Object.const_set ruby_class_name, klass
+puts ruby_class_name
+split.instance = Java::Sparky.serialize Object.const_get(ruby_class_name).new
+split.klass = File.read(java_class_path + '.class').to_java_bytes
+
+lines = context.textFile 'words.txt', 1
+result = lines.flatMap split
+
+result.collect.each {|result| p result}
